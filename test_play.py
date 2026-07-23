787da5f976df887043c62b2f29861869b59af643
prasadarao726 <prasadarao726@gmail.com>
2026-07-23 20:08:50 +0530

    second

--- test_play.py
+++ test_play.py
@@ -1,8 +1,10 @@
 from playwright.sync_api import sync_playwright
 import pytest
 
+@pytest.mark.home
 def test_m11():
     print("hello world")
 
+@pytest.mark.home
 def test_m12():
-    print("hello world hello world")    
+    print("hello world hello world")     
